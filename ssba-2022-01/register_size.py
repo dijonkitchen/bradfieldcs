be8f6d1da895fba8805d9080bc94dcc8273d4f17
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2022-01-02 11:55:15 -0800

    feat: add python register test

---
+++ ssba-2022-01/register_size.py
@@ -0,0 +1,13 @@
+print("1 << 64 is: {}".format(1 << 64))
+
+# n = 1
+#
+# for i in range(128):
+#     n = n << 1
+#     print(n)
+#
+#     if n == 0:
+#         print("Registers are {}".format(i + 1))
+#         break
+#
+# print("Not found.")
