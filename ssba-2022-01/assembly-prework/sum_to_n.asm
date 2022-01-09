65c4276144a932058512e7a0b314420933403a0a
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2022-01-09 15:39:14 -0800

    feat: add assembly sum

--- ssba-2022-01/assembly-prework/sum_to_n.asm
+++ ssba-2022-01/assembly-prework/sum_to_n.asm
@@ -1,4 +1,18 @@
 section .text
-global sum_to_n
-sum_to_n:
+    global sum_to_n
+
+sum_to_n: ; inclusive
+    mov rax, 0 ; sum
+    mov rbx, 0 ; count
+    jmp .compare
+
+.loop:
+    add rax, rbx
+    add rbx, 1
+    jmp .compare
+
+.compare:
+    cmp rbx, rdi
+    jle .loop
 	ret
+
