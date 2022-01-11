d0ac21cb55078d96d849fca6bfe714e2aaadd812
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2022-01-10 21:08:23 -0800

    feat: add binary conversion pseudocode

--- ssba-2022-01/assembly-prework/binary_convert.asm
+++ ssba-2022-01/assembly-prework/binary_convert.asm
@@ -1,4 +1,32 @@
 section .text
-global binary_convert
+    global binary_convert
+
 binary_convert:
+;#include <stdio.h>
+;#include <string.h>
+;#include <stdlib.h>
+;
+;int main(int argc, char* argv[]) {
+;    char* string = argv[0];
+;    int sum = 0;
+;    int powerOfTwo = 1;
+;
+;    for (int i = strlen(string) - 1; i >= 0; i--) {
+;        sum += (string[i] - '0') * powerOfTwo;
+;        powerOfTwo = powerOfTwo * 2;
+;    }
+;
+;    return sum;
+;}
+; intialize sum as zero
+; get string length (until null terminal value)
+; parse through string in reverse based on length, one char at a time
+; calculate power of 2 for place
+; multiply product by digit
+; add to sum
+; when no more digits,
+; return sum
+    mov rax, 0 ; sum
+    mov rbx, 1 ; initial power of 2^0
+
 	ret
