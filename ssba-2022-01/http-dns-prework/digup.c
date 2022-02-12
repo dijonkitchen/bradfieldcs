71d898939ba103f950a9e508efe00faf4a2a80a3
Jonathan Chen <11434205+dijonkitchen@users.noreply.github.com>
2022-02-12 13:45:28 -0800

    build: setup for dns lookup

---
+++ ssba-2022-01/http-dns-prework/digup.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    // create/open a socket
+    
+    // bind to a port
+    // encode query message
+    // send query message
+    // receive response
+    // decode response
+}
